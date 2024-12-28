b03896e7a4c393ed1dd405605d08ac6c739538dd
wdong218 <wdh8890@naver.com>
2024-12-28 19:50:51 +0900

    EMJA-11 : 딜러 ai, 및 게임 매니저, 게임 로직 구현 완료

--- GameManager.py
+++ GameManager.py
@@ -1,5 +1,23 @@
+from player import player
+from dealer import dealer
 class GameManager():
-    def __init__(self):
+    def __init__(self,player,dealer):
+        self.player = player
+        self.dealer = dealer
+        self.winner = None  # 최종 승자를 저장
+
+    def comparison(self):
+        player_score = self.player.get_player_score()
+        dealer_score = self.dealer.get_dealer_score()
+        if player_score == dealer_score:
+            self.winner = "draw"
+        elif player_score > dealer_score:
+            self.winner = "player"
+        else:
+            self.winner = "dealer"
+
+    def get_winner(self):
+        return self.winner
 
 
 
--- game_screen.py
+++ game_screen.py
@@ -4,6 +4,7 @@ from Deck import Deck
 from Card import Card
 from player import player
 from dealer import dealer
+from GameManager import GameManager
 class GameScreen:
     def __init__(self, screen):
         # 초기화
@@ -11,6 +12,7 @@ class GameScreen:
         self.font = pygame.font.Font(None, 30)
         self.deck = None  # 초기에는 None
         self.card = None
+        self.stand_push = False
         # 버튼 생성
         self.buttons = [
             Button(100, 500, 100, 40, "Start", (0, 0, 255), (0, 255, 0), (255, 255, 255), self.start_game),
@@ -34,23 +36,39 @@ class GameScreen:
         self.card = Card(self.deck)
         self.player = player()
         self.dealer = dealer()
+        self.stand_push = False
         print(self.deck.cards)
 
 
     def hit(self):
-        print("카드를 분배 중 입니다!")
-        player_card = self.card.player_Card_Hit()  # Deck에서 카드 한 장 뽑기
-        self.player.add_player_card(player_card)  # 카드 추가 및 점수 업데이트
-        dealer_card = self.card.dealer_Card_Hit()
-        self.dealer.add_dealer_card(dealer_card)
-        print("Player's cards:", self.player.get_player_card())
-        print("Player's score:", self.player.get_player_score())
-
-        print("Dealer's cards:", self.dealer.get_dealer_card())
-        print("Dealer's score:", self.dealer.get_dealer_score())
+        if self.stand_push == False:
+            print("카드를 분배 중 입니다!")
+            player_card = self.card.player_Card_Hit()  # Deck에서 카드 한 장 뽑기
+            self.player.add_player_card(player_card)  # 카드 추가 및 점수 업데이트
+            print(self.player.player_card)
+            print(self.player.player_score)
+            if self.player.get_player_score() > 21:
+                print("Player busted! Dealer wins!")
+                self.stand_push = True
+        else:
+            print("이미 종료된 게임입니다. Start 버튼을 눌러주세요")
 
     def stand(self):
-        print("Stand!")
+        while self.dealer.dealer_score < 17:
+            dealer_card = self.card.dealer_Card_Hit()
+            self.dealer.add_dealer_card(dealer_card)
+        if self.dealer.get_dealer_score() > 21:
+            print("Dealer busted! Player wins!")
+            print("Dealer card = ",self.dealer.dealer_card)
+            print("Dealer score = ",self.dealer.dealer_score)
+            self.stand_push = True
+        else:
+            self.GameManager = GameManager(self.player, self.dealer)
+            self.GameManager.comparison()
+            print("Dealer card = " , self.dealer.dealer_card)
+            print("Dealer score = " , self.dealer.dealer_score)
+            print("Winner : " , self.GameManager.get_winner())
+            self.stand_push = True
 
     # 이벤트 처리
     def handle_event(self, event):
