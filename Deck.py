c47e4b219427aedc031e6f0af2ad970538e91046
wdong218 <wdh8890@naver.com>
2024-12-30 16:47:26 +0900

    EMJA-16 : 이미지 삽입 구현 완료

--- Deck.py
+++ Deck.py
@@ -5,7 +5,7 @@ class Deck:
         self.cards = []
     def create_Deck(self): #카드 뭉치 생성
         print("카드 생성중...")
-        suits = ["♣", "♥", "♠", "♦"]  # 클로버, 하트, 스페이드, 다이아몬드
+        suits = ["C", "H", "S", "D"]  # 클로버, 하트, 스페이드, 다이아몬드
         ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
         self.cards = [rank + suit for suit in suits for rank in ranks]
     def shuffle(self): # 무작위 순서로 카드 섞기
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
-from game_screen import GameScreen  # GameScreen 클래스를 가져옴
+from game_screen import GameScreen
 
-# 초기화
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Blackjack - Card Layout Example")
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Blackjack Game")
 
 # GameScreen 초기화
 game_screen = GameScreen(screen)
@@ -15,14 +14,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        game_screen.handle_event(event)  # 이벤트 처리
 
-        # 이벤트 처리
-        game_screen.handle_event(event)
-
-    # 화면 업데이트 및 그리기
-    game_screen.update()
-    game_screen.draw()
-
-    pygame.display.flip()
+    game_screen.draw()  # 매 프레임 화면 업데이트
 
 pygame.quit()
